2b14a1a17b1280b98422bd66cc55d75695e05605
andrew-programs <andrew.lloyd.williams@outlook.com>
2023-01-09 20:12:51 -0700

    update to page

---
+++ js/TaxEstimator.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded',() => {
+  var eRate = document.getElementById('rate');
+  var eHours = document.getElementById('hours');
+  var weekly = document.getElementById('weekly');
+  var monthly = document.getElementById('monthly');
+  var annual = document.getElementById('annual');
+
+  eRate.addEventListener("input", () => {
+    let annualIncome = annual.value;
+
+    
+  });
+
+  eHours.addEventListener("input", () => {
+
+  });
+});
